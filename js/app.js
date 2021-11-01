47ad651e39b2a161f0b0f2bee536d66cd0f576a0
sherylmoser <sherylrmoser@gmail.com>
2021-11-01 10:29:39 -0600

    fixed course ID selection problem

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
 function getAvailableCourses() {
-    return new Promise((resolve, reject) => {
-        fetch('https://golf-courses-api.herokuapp.com/courses/')
-            .then(response => response.json())
-            .then(data => resolve(render(data.courses)));
-    });
+    return fetch('https://golf-courses-api.herokuapp.com/courses/')
+        .then(response => response.json())
+        .then(data => courseSelectionRender(data.courses));
 }
-function render(courses) {
+function courseSelectionRender(courses) {
     let courseSelectionHTML = '';
     courses.forEach((course) => {
         courseSelectionHTML += `<option value="${course.id}">${course.name}</option>`
     })
     document.getElementById('courseSelect').innerHTML = courseSelectionHTML;
 }
-getAvailableCourses()
+getAvailableCourses();
 
-let selectedCourse = document.getElementById('courseSelect').selectedIndex.text;
-
-
-function getAvailableTees() {
-    return new Promise((resolve, reject) => {
-        fetch()
+function onCourseSelection(event) {
+    const courseId = event.target.value
+    getCourseInfo(courseId).then(data => {
+        const course = data.data;
     })
 }
+
+
+function getCourseInfo(courseId) {
+    return fetch(`https://golf-courses-api.herokuapp.com/courses/${courseId}`)
+        .then((response) => response.json());
+}
+// let coursePromise = getCourseInfo();
+// coursePromise.then(data => {
+//     const course = data.data;
+//     course.holes.forEach(hole => {
+//         console.log(`${hole.teeBoxes[0].par}`)
+//         console.log(`${hole.teeBoxes[0].yards}`)
+//         console.log(`${hole.teeBoxes[0].hcp}`)
+//     })
+// })
