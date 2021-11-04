5dc6ca6f98e2e8af0f85062769955f471dcefa98
sherylmoser <sherylrmoser@gmail.com>
2021-11-04 10:04:10 -0600

    table render functions done

--- js/app.js
+++ js/app.js
@@ -24,16 +24,16 @@ function onCourseSelection(event) {
         const course = data.data;
         const holes = course.holes;
         let teeSelectionHTML = '<option value="0">Select Your Tee</option>';
-        holes[0].teeBoxes.forEach(teeBox => {
-            teeSelectionHTML += `<option value="${teeBox.teeTypeId}">${teeBox.teeType}</option>`;
+        holes[0].teeBoxes.forEach((teeBox, index) => {
+            teeSelectionHTML += `<option value="${index}">${teeBox.teeType}</option>`;
         });
         document.getElementById('teeSelect').innerHTML = teeSelectionHTML;
     });
 }
 
 function saveTeeSelection(event) {
-    const teeId = event.target.value;
-    localStorage.setItem('teeId', teeId)
+    const teeTypeIndex = event.target.value;
+    localStorage.setItem('teeTypeIndex', teeTypeIndex)
 }
 
 class Player {
@@ -56,47 +56,111 @@ function addPlayer(event) {
     save()
 }
 
-function courseRetrieve() {
-    return localStorage.getItem('courseId')
+function yardsRender() {
+    const currentCourseId = localStorage.getItem('courseId');
+    const currentTeeType = localStorage.getItem('teeTypeIndex');
+    getCourseInfo(currentCourseId).then(data => {
+        const course = data.data;
+        const holes = course.holes;
+        let frontNineYardsHTML = '<th scope="row">Yards</th>';
+        for (let i = 0; i < 9; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const yards = teeType.yards;
+            frontNineYardsHTML += `<td scope="row">${yards}</td>`;
+        }
+        frontNineYardsHTML += '<th id="frontYardsOut" scope="row"></th>'
+        document.getElementById('frontYards').innerHTML = frontNineYardsHTML;
+        let backNineYardsHTML = '<th scope="row">Yards</th>';
+        for (let i = 9; i < 18; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const yards = teeType.yards;
+            backNineYardsHTML += `<td scope="row">${yards}</td>`;
+        }
+        backNineYardsHTML += `<th id="backYardsIn" scope="row"></th>
+            <th id="backYardsTotal" scope="row"></th>`;
+        document.getElementById('backYards').innerHTML = backNineYardsHTML;
+    });
 }
 
-const courseId = courseRetrieve();
-
-function teeTypeRetrieve() {
-    return localStorage.getItem('teeId')
+function parRender() {
+    const currentCourseId = localStorage.getItem('courseId');
+    const currentTeeType = localStorage.getItem('teeTypeIndex');
+    getCourseInfo(currentCourseId).then(data => {
+        const course = data.data;
+        const holes = course.holes;
+        let frontNineParHTML = '<th scope="row">Par</th>';
+        for (let i = 0; i < 9; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const par = teeType.par;
+            frontNineParHTML += `<td scope="row">${par}</td>`;
+        }
+        frontNineParHTML += '<th id="frontParOut" scope="row"></th>'
+        document.getElementById('frontPar').innerHTML = frontNineParHTML;
+        let backNineParHTML = '<th scope="row">Par</th>';
+        for (let i = 9; i < 18; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const par = teeType.par;
+            backNineParHTML += `<td scope="row">${par}</td>`;
+        }
+        backNineParHTML += `<th id="backParIn" scope="row"></th>
+            <th id="backParTotal" scope="row"></th>`;
+        document.getElementById('backPar').innerHTML = backNineParHTML;
+    });
 }
 
-const teeTypeId = teeTypeRetrieve();
+function handicapRender() {
+    const currentCourseId = localStorage.getItem('courseId');
+    const currentTeeType = localStorage.getItem('teeTypeIndex');
+    getCourseInfo(currentCourseId).then(data => {
+        const course = data.data;
+        const holes = course.holes;
+        let frontNineHcpHTML = '<th scope="row">Handicap</th>';
+        for (let i = 0; i < 9; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const hcp = teeType.hcp;
+            frontNineHcpHTML += `<td scope="row">${hcp}</td>`;
+        }
+        frontNineHcpHTML += '<th id="frontHcpOut" scope="row"></th>'
+        document.getElementById('frontHandicap').innerHTML = frontNineHcpHTML;
+        let backNineHcpHTML = '<th scope="row">Handicap</th>';
+        for (let i = 9; i < 18; i++) {
+            const teeType = holes[i].teeBoxes[currentTeeType];
+            const hcp = teeType.hcp;
+            backNineHcpHTML += `<td scope="row">${hcp}</td>`;
+        }
+        backNineHcpHTML += `<th id="backHcpIn" scope="row"></th>
+            <th id="backHcpTotal" scope="row"></th>`;
+        document.getElementById('backHandicap').innerHTML = backNineHcpHTML;
+    });
+}
 
 function playerRender() {
     players.forEach(player => {
         let frontNinePlayerRowHTML = document.createElement('tr')
-        frontNinePlayerRowHTML.innerHTML = `<th scope="row">${player.name}</th>
-            <td><input id="${player.name}Scores-1" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-2" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-3" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-4" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-5" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-6" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-7" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-8" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-9" class="hitsInput" type="number"></td>`;
+        frontNinePlayerRowHTML.innerHTML = `<th scope="row">${player.name}</th>`;
+        for (let i = 1; i < 10; i++) {
+            frontNinePlayerRowHTML.innerHTML += `<td><input id="${player.name}Score${i}" class="hitsInput" type="number"></td>`;
+        }
+        frontNinePlayerRowHTML.innerHTML += `<td id="${player.name}Out"></td>`;
         document.getElementById('frontNine').appendChild(frontNinePlayerRowHTML);
         let backNinePlayerRowHTML = document.createElement('tr')
-        backNinePlayerRowHTML.innerHTML = `<th scope="row">${player.name}</th>
-            <td><input id="${player.name}Scores-10" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-11" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-12" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-13" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-14" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-15" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-16" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-17" class="hitsInput" type="number"></td>
-            <td><input id="${player.name}Scores-18" class="hitsInput" type="number"></td>`;
+        backNinePlayerRowHTML.innerHTML = `<th scope="row">${player.name}</th>`;
+        for (let i = 10; i < 19; i++) {
+            backNinePlayerRowHTML.innerHTML += `<td><input id="${player.name}Score${i}" class="hitsInput" type="number"></td>`
+        }
+        backNinePlayerRowHTML.innerHTML += `<td id="${player.name}Total"></td>`;
         document.getElementById('backNine').appendChild(backNinePlayerRowHTML);
     })
 }
+function tableRender() {
+    yardsRender();
+    parRender();
+    handicapRender();
+    playerRender();
+}
+function addScore() {
 
+}
 
 function save() {
     const playersArrayString = JSON.stringify(players);
@@ -116,15 +180,3 @@ function playerRetrieve() {
     }
     return []
 }
-
-
-
-// let coursePromise = getCourseInfo();
-// coursePromise.then(data => {
-//     const course = data.data;
-//     course.holes.forEach(hole => {
-//         console.log(`${hole.teeBoxes[0].par}`)
-//         console.log(`${hole.teeBoxes[0].yards}`)
-//         console.log(`${hole.teeBoxes[0].hcp}`)
-//     })
-// })
